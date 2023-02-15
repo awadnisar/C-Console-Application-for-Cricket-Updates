f957be811a46ad7c9c1d98dab33523d7773106dc
Awad Nisar <45533871+awadnisar@users.noreply.github.com>
2023-02-16 01:56:35 +0500

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Lab_Project
+{
+    internal class Score
+    {
+        public int runs, wickets;
+        public double overs, totalOvers;
+        
+        public Score(int runs, int wickets, double overs, double totalOvers)
+        {
+            this.runs = runs;
+            this.wickets = wickets;
+            this.overs = overs;
+            this.totalOvers = totalOvers;
+        }
+    }
+}
